dfe9ce9899f7912688c84f86b087f5b3130fbcdc
Igor Makeev <makeevigor@mail.ru>
2021-06-29 08:49:20 +0300

    Initial Commit

---
+++ GetYes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetYes
+//
+//  Created by Igor Makeev on 29.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
