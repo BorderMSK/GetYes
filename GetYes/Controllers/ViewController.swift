2318b400ee513d31533b9b5cdc7154ba35bbe827
Igor Makeev <makeevigor@mail.ru>
2021-06-30 15:39:54 +0300

    add code fix

--- GetYes/Controllers/ViewController.swift
+++ GetYes/Controllers/ViewController.swift
@@ -10,43 +10,17 @@ import FLAnimatedImage
 
 class ViewController: UIViewController {
     
-    @IBOutlet var imageView: FLAnimatedImageView!
-    @IBOutlet var answerLabel: UILabel!
+    var data = NetWork()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .black
-        answerLabel.isHidden = true
-    }
-    
-    func getImage() {
-        
-        guard let url = URL(string: "https://yesno.wtf/api") else { return }
-        
-        URLSession.shared.dataTask(with: url) { ( data, _, _ ) in
-            guard let data = data else { return }
-            
-            do {
-                let results = try JSONDecoder().decode(Model.self, from: data)
-                DispatchQueue.global().async {
-                    guard let url = URL(string: results.image) else { return }
-                    guard let imageData = try? Data(contentsOf: url) else { return }
-                    
-                    DispatchQueue.main.async {
-                        self.imageView.animatedImage = FLAnimatedImage(animatedGIFData: imageData)
-                        self.answerLabel.text = results.answer
-                        self.answerLabel.isHidden = false
-                    }
-                }
-            } catch let error {
-                print(error)
-            }
-        }.resume()
+        data.answerLabel.isHidden = true
     }
     
     @IBAction func tappedButton(_ sender: UIButton) {
 
-        getImage()
+        data.getImage()
     }
 }
