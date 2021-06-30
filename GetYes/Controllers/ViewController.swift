ff24d25a2af408ab59a972302ddb12bc2361bcee
Igor Makeev <makeevigor@mail.ru>
2021-06-30 17:22:44 +0300

    add Alamofire in project

--- GetYes/Controllers/ViewController.swift
+++ GetYes/Controllers/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FLAnimatedImage
+import Alamofire
 
 class ViewController: UIViewController {
     
@@ -15,7 +16,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .black
         answerLabel.isHidden = true
     }
@@ -23,30 +24,28 @@ class ViewController: UIViewController {
     func getImage() {
         
         guard let url = URL(string: "https://yesno.wtf/api") else { return }
-        
-        URLSession.shared.dataTask(with: url) { ( data, _, _ ) in
-            guard let data = data else { return }
-            
-            do {
-                let results = try JSONDecoder().decode(Model.self, from: data)
+
+        AF.request(url).validate().responseDecodable(of: Model.self) { response in
+            switch response.result {
+            case .success(let data):
                 DispatchQueue.global().async {
-                    guard let url = URL(string: results.image) else { return }
+                    guard let url = URL(string: data.image!) else { return }
                     guard let imageData = try? Data(contentsOf: url) else { return }
-                    
-                    DispatchQueue.main.async {
-                        self.imageView.animatedImage = FLAnimatedImage(animatedGIFData: imageData)
-                        self.answerLabel.text = results.answer
-                        self.answerLabel.isHidden = false
-                    }
+                DispatchQueue.main.async {
+                    self.imageView.animatedImage = FLAnimatedImage(animatedGIFData: imageData)
+                    self.answerLabel.text = data.answer
+                    self.answerLabel.isHidden = false
                 }
-            } catch let error {
+                }
+                
+            case .failure(let error):
                 print(error)
             }
-        }.resume()
+        }
     }
     
     @IBAction func tappedButton(_ sender: UIButton) {
-
         getImage()
     }
 }
+
--- GetYes/Model/Model.swift
+++ GetYes/Model/Model.swift
@@ -6,7 +6,22 @@
 //
 
 struct Model: Decodable {
-    let answer: String
-    let forced: Bool
-    let image: String
+    let answer: String?
+    let forced: Bool?
+    let image: String?
+    
+    init(imageData: [String: Any]) {
+        answer = imageData["answer"] as? String
+        forced = imageData["forced"] as? Bool
+        image = imageData["image"] as? String
+    }
+    
+    static func getImages(from value: Any) -> [Model]? {
+        guard let imageData = value as? [[String: Any]] else {
+            return nil
+        }
+        return imageData.compactMap {
+            Model(imageData: $0)
+        }
+    }
 }
