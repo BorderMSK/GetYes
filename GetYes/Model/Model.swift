86076e6b7bc71241a0067ec951bf56e1ec538bd0
Igor Makeev <makeevigor@mail.ru>
2021-06-30 15:16:44 +0300

    add new app

---
+++ GetYes/Model/Model.swift
@@ -0,0 +1,12 @@
+//
+//  Model.swift
+//  GetYes
+//
+//  Created by Igor Makeev on 29.06.2021.
+//
+
+struct Model: Decodable {
+    let answer: String
+    let forced: Bool
+    let image: String
+}
